69325f5a0ff6e03fbc507448cd8ffa38e57bc38e
Victor Henrique <you@example.com>
2020-06-07 04:03:47 -0300

    Primeiro commit

---
+++ server/src/routes.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import knex from './database/connection';
+
+const routes = express.Router();
+
+// routes.get('/' , (request, response) => {
+//     return response.json({message: 'Hello World'});
+// });
+
+routes.get('/items' , async(request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.name,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+
+    return response.json(items);
+});
+
+
+export default routes;
