ed50fb25177e681de8757fe365b7f5f02ff7de04
Victor Henrique <you@example.com>
2020-06-07 14:39:18 -0300

    Novas alterações nas rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,24 +1,17 @@
 import express from 'express';
-import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-// routes.get('/' , (request, response) => {
-//     return response.json({message: 'Hello World'});
-// });
+routes.get('/items' , itemsController.index );
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
-routes.get('/items' , async(request, response) => {
-    const items = await knex('items').select('*');
-
-    const serializedItems = items.map(item => {
-        return {
-            title: item.name,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        };
-    });
-
-    return response.json(items);
-});
 
 
 export default routes;
